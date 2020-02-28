3b9d9c52879c2a542efb6e22f8fdbbf6d3f18bd9
zhousijian <13553593242@163.com>
2020-02-28 19:20:24 +0800

    完成首页轮播图基本静态结构和数据渲染

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //app.js
+// 引入封装好的request文件
+import request from './utils/request.js'
 App({
   onLaunch: function () {
+    // 设置基准路径
+    request.defaults.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,21 @@
 //index.js
+// 引入request文件
+import request from '../../utils/request.js'
 Page({
- 
+  data : {
+    // 轮播图的数据
+    message : []
+  },
+  onLoad(){
+    // 轮播图的请求
+    request({
+      url: '/home/swiperdata'
+    }).then(res=>{
+      // console.log(res)
+      const { message } = res.data
+      this.setData({
+        message: message
+      })
+    })
+  }
 })
