97dda989b60aecb07e2d2e202d531730854bb179
zhousijian <13553593242@163.com>
2020-02-29 21:47:10 +0800

    完成主体部分的左侧菜单栏的基本静态结构和数据渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,37 @@
 // pages/category/index.js
+// 引入request文件
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 页面全部的数据
+    allData : [],
+    // 点击菜单获取的对应的索引
+    current : 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(){
+    request({
+      url: '/categories'
+    }).then(res=>{
+      // console.log(res)
+      const { message } = res.data
+      this.setData({
+        allData : message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleClick(e){
+    // console.log(e)
+    this.setData({
+      current : e.currentTarget.dataset.index
+    })
   }
+
 })
