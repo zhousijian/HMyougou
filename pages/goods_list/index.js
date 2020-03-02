ff1017ce0acbdffbeb5f2753b2c74b8ae81482a0
zhousijian <13553593242@163.com>
2020-03-02 21:14:16 +0800

    完成商品列表搜索页的商品列表项基本结构和数据渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,33 @@
 // pages/goods_list/index.js
+import request from '../../utils/request.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 搜索返回的数据
+    allData: {},
+    // 关键字
+    keyword : '',
+    // 页码
+    pagenum: 1
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // console.log(options)
+    this.setData({
+      keyword: options.keyword
+    })
+    request({
+      url: '/goods/search',
+      data: {
+        query: this.data.keyword,
+        pagesize : 10,
+        pagenum: this.data.pagenum
+      }
+    }).then(res => {
+      // console.log(res)
+      const { message } = res.data
+      this.setData({
+        allData: message
+      })
+    })
   }
 })
