1d26c5a91555754b4a5bf9165e08c6bc408780be
zhousijian <13553593242@163.com>
2020-03-03 21:46:03 +0800

    完成搜索页的搜索框input事件请求和防抖节流

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,76 @@
 // pages/search/index.js
+import request from '../../utils/request.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    // 输入框的值
+    value: '',
+    // 输入框最后输入的值
+    finallyValue: '',
+    // 搜索请求返回的数据
+    searchData: [],
+    // 是否正在请求
+    load: false
+  },
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // input事件
+  handleInput(e) {
+    // console.log(e)
+    // 获取输入框的值
+    let {
+      value
+    } = e.detail
+    this.setData({
+      value
+    })
+    if (!this.data.value) {
+      this.setData({
+        searchData: []
+      })
+      return
+    };
+    // 搜索请求
+    this.getData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 点击取消触发的事件
+  handleCancel(){
+    this.setData({
+      value : '',
+      searchData : []
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 搜索请求的封装
+  getData() {
+    // 判断是否正在请求
+    if (this.data.load) return;
+    this.setData({
+    // 如果可以进来，说明不是请求中，把状态改为true
+      load: true,
+      // 保存当前搜索的值
+      finallyValue : this.data.value
+    })
+    request({
+      url: '/goods/qsearch',
+      data: {
+        query: this.data.value
+      }
+    }).then(res => {
+      // console.log(res)
+      const {
+        message
+      } = res.data
+      this.setData({
+        searchData: message,
+        // 请求完毕把状态改为false
+        load: false
+      })
+      // console.log(this.data.value)
+      // console.log(this.data.finallyValue)
+      if (this.data.value != this.data.finallyValue && this.data.value) {
+        this.getData()
+      }
+    })
   }
 })
