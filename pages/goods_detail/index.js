f9a665b28bd2f6dc174904b1998fcd2f663740fa
zhousijian <13553593242@163.com>
2020-03-05 02:31:47 +0800

    完成轮播图的图片预览功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -4,7 +4,9 @@ Page({
 
   data: {
     // 商品详情信息
-    goodsDetailInfo : {}
+    goodsDetailInfo : {},
+    // 轮播图的图片地址数组
+    images : []
   },
   onLoad: function (options) {
     request({
@@ -12,10 +14,23 @@ Page({
       data: { goods_id : options.id}
     }).then(res=>{
       // console.log(res)
-      const { message } = res.data
-      this.setData({
-        goodsDetailInfo : message
+      let { message } = res.data
+      let images = message.pics.map(v=>{
+        return v.pics_big
       })
+      this.setData({
+        goodsDetailInfo : message,
+        images
+      })
+    })
+  },
+  // 点击图片预览
+  handleClickImage(e){
+    const { index } = e.currentTarget.dataset
+    // console.log(index)
+    wx.previewImage({
+      current: this.data.images[index], // 当前显示图片的http链接
+      urls: this.data.images // 需要预览的图片http链接列表
     })
   }
 })
