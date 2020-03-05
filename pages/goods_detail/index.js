58633ecc7e790e68d6731be3563662579d7e1aa0
zhousijian <13553593242@163.com>
2020-03-05 13:32:53 +0800

    完成商品详情页的所有静态结构和数据渲染，（除联系客服功能外）

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -4,33 +4,43 @@ Page({
 
   data: {
     // 商品详情信息
-    goodsDetailInfo : {},
+    goodsDetailInfo: {},
     // 轮播图的图片地址数组
-    images : []
+    images: [],
+    // tab栏的索引
+    current : 0
   },
   onLoad: function (options) {
     request({
       url: '/goods/detail',
-      data: { goods_id : options.id}
-    }).then(res=>{
+      data: { goods_id: options.id }
+    }).then(res => {
       // console.log(res)
       let { message } = res.data
-      let images = message.pics.map(v=>{
+      let images = message.pics.map(v => {
         return v.pics_big
       })
       this.setData({
-        goodsDetailInfo : message,
+        goodsDetailInfo: message,
         images
       })
     })
   },
   // 点击图片预览
-  handleClickImage(e){
+  handleClickImage(e) {
     const { index } = e.currentTarget.dataset
     // console.log(index)
     wx.previewImage({
       current: this.data.images[index], // 当前显示图片的http链接
       urls: this.data.images // 需要预览的图片http链接列表
+    })
+  },
+  // 点击切换tab栏对应信息
+  handleTab(e) {
+    // console.log(e)
+    const { index } = e.currentTarget.dataset
+    this.setData({
+      current : index
     })
   }
 })
