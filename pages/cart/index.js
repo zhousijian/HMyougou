f0b74c1c9f3733bf63b1097a68c75a66f5d4e441
zhousijian <13553593242@163.com>
2020-03-06 23:48:38 +0800

    完成了在商品详情页点击加入购物车，保存商品数据到本地存储和购物车页面的的商品数量加减的价格计算

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,14 +3,28 @@ Page({
 
   data: {
     // 收货信息
-    gainInfo: {}
+    gainInfo: {},
+    // 本地加入购物车的商品
+    storageGoods: [],
+    // 总价格
+    allPrice: 0
   },
-
-  onLoad: function (options) {
+  // 页面一开始加载时候触发，只触发一次
+  onLoad: function(options) {
     // 从本地查看有没有收货地址
     this.setData({
       gainInfo: wx.getStorageSync('address')
     })
+  },
+  // 页面每次刷新和切换都会触发
+  onShow() {
+    // 从本地获取加入购物车的商品
+    // console.log(wx.getStorageSync('goodsInfoArr'))
+    this.setData({
+      storageGoods: wx.getStorageSync('goodsInfoArr')
+    })
+    // 计算总价格
+    this.computedAllPrice()
   },
   // 点击获取收货地址
   handleGainAddress() {
@@ -27,6 +41,66 @@ Page({
         // 把收货地址保存到本地
         wx.setStorageSync('address', this.data.gainInfo)
       }
+    })
+  },
+  // 添加或删减购物车商品的数量
+  handleAlterAmount(e){
+    const { index,amount } = e.currentTarget.dataset
+    // console.log(index)
+    let arr = wx.getStorageSync('goodsInfoArr')
+    arr[index].goods_number += amount
+    arr.forEach((v,i)=>{
+      if (v.goods_number == 0) {
+        wx.showModal({
+          title: '提示',
+          content: '您确定需要删除该商品吗？',
+          success :(res)=> {
+            if (res.confirm) {
+              arr.splice(i, 1)
+              // this.setData({
+              //   storageGoods: arr
+              // })
+              // wx.setStorageSync('goodsInfoArr', arr)
+              // this.computedAllPrice()
+            } else if (res.cancel) {
+              v.goods_number = 1
+              // this.setData({
+              //   storageGoods: arr
+              // })
+              // wx.setStorageSync('goodsInfoArr', arr)
+              // this.computedAllPrice()
+            }
+            this.setData({
+              storageGoods: arr
+            })
+            wx.setStorageSync('goodsInfoArr', arr)
+            this.computedAllPrice()
+          }
+        })
+      }else {
+        this.setData({
+          storageGoods: arr
+        })
+        wx.setStorageSync('goodsInfoArr', arr)
+        this.computedAllPrice()
+      }
+    })
+    // console.log(arr)
+    // this.setData({
+    //   storageGoods: arr
+    // })
+    // wx.setStorageSync('goodsInfoArr', arr)
+    // this.computedAllPrice()
+  },
+  // 计算总价格封装函数
+  computedAllPrice() {
+    let price = 0
+    if (!this.data.storageGoods) return;
+    this.data.storageGoods.forEach(v => {
+      price += v.goods_price * v.goods_number
+    })
+    this.setData({
+      allPrice : price
     })
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -8,15 +8,19 @@ Page({
     // 轮播图的图片地址数组
     images: [],
     // tab栏的索引
-    current : 0
+    current: 0
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     request({
       url: '/goods/detail',
-      data: { goods_id: options.id }
+      data: {
+        goods_id: options.id
+      }
     }).then(res => {
       // console.log(res)
-      let { message } = res.data
+      let {
+        message
+      } = res.data
       let images = message.pics.map(v => {
         return v.pics_big
       })
@@ -28,7 +32,9 @@ Page({
   },
   // 点击图片预览
   handleClickImage(e) {
-    const { index } = e.currentTarget.dataset
+    const {
+      index
+    } = e.currentTarget.dataset
     // console.log(index)
     wx.previewImage({
       current: this.data.images[index], // 当前显示图片的http链接
@@ -38,9 +44,50 @@ Page({
   // 点击切换tab栏对应信息
   handleTab(e) {
     // console.log(e)
-    const { index } = e.currentTarget.dataset
+    const {
+      index
+    } = e.currentTarget.dataset
     this.setData({
-      current : index
+      current: index
     })
+  },
+  // 点击加入购物车把商品信息保存到本地
+  handleAddCart() {
+    // console.log(this.data.goodsDetailInfo)
+    // 定义保存商品信息
+    let goodsInfo = {};
+    goodsInfo.goods_id = this.data.goodsDetailInfo.goods_id;
+    goodsInfo.goods_name = this.data.goodsDetailInfo.goods_name;
+    goodsInfo.goods_price = this.data.goodsDetailInfo.goods_price;
+    goodsInfo.goods_big_logo = this.data.goodsDetailInfo.goods_big_logo;
+    goodsInfo.goods_number = 1;
+    // console.log(goodsInfo);
+    // 查询本地是否有加入过该商品
+    let arr = wx.getStorageSync('goodsInfoArr')
+    if (!Array.isArray(arr)) {
+      arr = []
+    }
+    let result = arr.some(v => {
+      return v.goods_id == goodsInfo.goods_id
+    })
+    if(result){
+      arr.forEach(v=>{
+        if (v.goods_id == goodsInfo.goods_id){
+          ++v.goods_number
+          wx.showToast({
+            title: '该商品数量+1',
+            icon: 'success'
+          })
+        }
+      })
+    }else {
+      wx.showToast({
+        title: '已添加购物车',
+        icon: 'success'
+      })
+      arr.unshift(goodsInfo)
+    }
+    wx.setStorageSync('goodsInfoArr', arr)
+    // console.log(result)
   }
 })
