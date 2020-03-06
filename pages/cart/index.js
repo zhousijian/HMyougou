cc299cd7592a9cb51a71a8e11db6401f125894ee
zhousijian <13553593242@163.com>
2020-03-06 21:37:19 +0800

    完成收货地址的获取，本地存储和数据渲染

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,32 @@
 // pages/cart/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 收货信息
+    gainInfo: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 从本地查看有没有收货地址
+    this.setData({
+      gainInfo: wx.getStorageSync('address')
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击获取收货地址
+  handleGainAddress() {
+    wx.chooseAddress({
+      success: (res) => {
+        this.setData({
+          gainInfo: {
+            userName: res.userName,
+            telNumber: res.telNumber,
+            address: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        // console.log(this.data.gainInfo)
+        // 把收货地址保存到本地
+        wx.setStorageSync('address', this.data.gainInfo)
+      }
+    })
   }
 })
